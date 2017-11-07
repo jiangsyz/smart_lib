d9ea8bf36006f9fcab3eafb8086aa9ec465aea6e
jiangsyz <305460883@qq.com>
2017-11-07 16:24:07 +0800

    smart_shell

--- smart_shell/smart_shell.cpp
+++ smart_shell/smart_shell.cpp
@@ -1,5 +1,26 @@
 #include "smart_shell.h"
 //=======================================
-bool sh_shell(string command){}
+bool sh_shell(string command){
+	//打开管道,执行命令
+	auto popen_fd=popen(command.c_str(),"r"); 
+	if(!popen_fd) return false;
+	//关闭管道
+	pclose(popen_fd); 
+	return true;
+}
 //=======================================
-bool sh_shell(string command,struct shell_output &output){}
+bool sh_shell(string command,string &output){
+	//初始化
+	output.assign("");
+	//打开管道
+	auto popen_fd=popen(command.c_str(),"r");
+	if(!popen_fd) return false;
+	//读取
+	char buf[sizeof(char)*1024]={'\0'};
+	while(fread(buf,sizeof(char),1023,popen_fd)) output+=buf;
+	//关闭管道
+	pclose(popen_fd);
+	//去除最后一个回车符
+	output.replace(output.length()-sizeof(char),sizeof(char),"");
+	return true;
+}
--- smart_shell/smart_shell.h
+++ smart_shell/smart_shell.h
@@ -1,19 +1,17 @@
 /*
 执行shell的一些方法
+g++ -g -gdwarf-2 -std=c++11 -shared -fpic -o libsmartshell.so smart_shell.cpp
 */
 #ifndef SMART_SHELL_H
 #define SMART_SHELL_H
+#include <algorithm>
+#include <string.h>
 using namespace std;
-//=======================================
-struct shell_output{
-	string standard_output;
-	string error_output;
-};
 //=======================================
 //作用：执行一个shell命令
 bool sh_shell(string command);
 //=======================================
 //作用：执行一个shell命令,并获取输出
-bool sh_shell(string command,struct shell_output &output);
+bool sh_shell(string command,string &output);
 //=======================================
 #endif
